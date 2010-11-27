e030ffb90f1c7ce398770bac510760b045090df9
Samson Nguyen <wise_old_dragon@hotmail.com>
2010-11-27 11:29:40 -0700

    Changed login form to conform to template Fixed gender formatting

--- includes/login.php
+++ includes/login.php
@@ -1,11 +1,15 @@
 <!-- Login form -->
 <form name="form1" method="post" action="checklogin.php">
-	<fieldset>
-		<legend>Please login to access the database</legend>
-		<label for="myusername">Username</label><br />
-		<input name="myusername" type="text" id="myusername"><br />
-		<label for="mypassword">Password</label><br />
-		<input name="mypassword" type="password" id="mypassword"><br />
+    <fieldset>
+    <legend>Login</legend>
+   		<div class="fm-req">
+			<label for="myusername">Username</label>
+			<input name="myusername" type="text" id="myusername">
+		</div>
+		<div class="fm-req">
+			<label for="mypassword">Password</label>
+			<input name="mypassword" type="password" id="mypassword">
+		</div>
 		<input type="submit" name="Submit" value="Login"> 
 	</fieldset>
 </form>
