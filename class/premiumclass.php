a1e19f788e91930329b5b6fdbb79e3a880fe66bf
Samson Nguyen <wise_old_dragon@hotmail.com>
2010-12-02 19:49:05 -0700

    Added a template premiumclass for calculating premiums and risks

---
+++ class/premiumclass.php
@@ -0,0 +1,36 @@
+<?php
+class premiumClass{
+	
+	/**
+	 * find all the tickets, and history of the client
+	 * Set the risk into the client
+	 * @param unknown_type $clientID
+	 */
+	function calculateClientRisk($clientID){
+		
+	}
+	
+	/**
+	 * For policy_no, we find all clients and vehicles listed under the policy
+	 * calculate the premium 
+	 */
+	function calculatePremium($policy_no){
+		
+	}
+	
+	/**
+	 * for vehicle vin, calculate it's risk based on ave_daily_miles, type, and value
+	 */
+	function calculateVehicleRisk($vin){
+		
+	}
+	
+	/**
+	 * Update premiums for ALL policies, WARNING THIS MAY TAKE A LONG TIME
+	 * Enter description here ...
+	 */
+	function batchPremiumUpdate(){
+		
+	}
+}
+?>
