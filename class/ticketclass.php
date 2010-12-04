28953f69c85cb4fb3a26e8266cb0a9251f614ef8
yussufa <yussufa@zone46ea.(none)>
2010-12-03 18:49:12 -0700

    more code for tickets

--- class/ticketclass.php
+++ class/ticketclass.php
@@ -1,7 +1,7 @@
 <?php
 class Ticket{
 
-function addNewTicket($array){
+		function addNewTicket($array){
 
 		//print "<BR/>";
 		$keys = array_keys($array); //Return the keys of the array;
@@ -26,7 +26,66 @@ function addNewTicket($array){
 		mysql_query($sql) or die(mysql_error());
 		return true;
 
-}
+		}
+
+		
+		//delete a ticket from the databse	
+		function deleteTicket($infraction_no){
+		$sql="DELETE FROM Ticket WHERE Infraction_No='$infraction_no'";
+		mysql_query($sql) or die(mysql_error());
+		return true;
+		}
+		
+		//update ticket information
+		function updateTicket($infraction_no,$newTicketInfo){
+		echo 'update ticket';
+		}
+		
+		//prints the form for update
+		function printUpdateForm($infraction_no){
+		echo 'print update form';
+		}
+		
+
+
+		//list the tickets in a table format
+		/**
+	 	* Prints out a simple list of tickets, sort by infraction number, also display the option to update, 		 	  delete, etc.
+	 	* @param offset Being where in the results
+	 	* @param limit Limit the number of results
+	 	*/
+		function listTickets($offset,$limit){
+		$returnString = array();
+		$sql = "SELECT * FROM Ticket ORDER BY Infraction_No ASC LIMIT $offset, $limit";
+		$result = mysql_query($sql);
+
+		print "<table class=\"tickets\"><tr><td>Infraction Number</td><td>Client ID</td><td>Officer Name</td><td>Officer Number</td><td>Classification</td><td>Date</td></tr>";
+
+		while($info = mysql_fetch_array($result)){
+			Print "<tr><td>";
+			if (($info['Infraction_No']!=null) || ($info['Infraction_No']!=0)){
+				print "<a href=\"ticket.php?action=update&ticket=".$info['Infraction_No']."\">".$info['Infraction_No']."</a>";
+			} else {
+				print $info['Infraction_No'];
+			}
+			print "</td><td>".$info['Client_ID']."</td><td>".$info['Officer_Name']."</td><td>".$info['Officer_No']."</td><td>".$info['Classification']."</td><td>".$info["Date"]."</td>";
+			$this->printOptions($info['Infraction_No']);
+			print "</tr>";
+		}
+		print "</table>";
+		}
+
+		function printOptions($infraction_no){
+		print "<td><a href=\"tickets.php?action=remove&ticket=$infraction_no\">X</a></td><td> <a href=\"tickets.php?action=update&ticket=$infraction_no\">Update</a></td>\n";
+		}
+
+		function totalTickets(){
+
+		$data = mysql_query("SELECT * FROM Ticket") or die(mysql_error());
+		return mysql_num_rows($data); //count the number of results and return
+		}
+
+
 
 }
 
--- tickets.php
+++ tickets.php
@@ -25,10 +25,66 @@ include $includesfolder.'header.php';
 		}else {
 			include $includesfolder.'addticket.php';
 
-		}}else if($_GET['action']=='search'){
-			echo 'search ticket';
-		}else { 
-			echo 'tickets';
 		}
-}
+
+
+		}else if($_GET['action']=='search'){
+			echo 'search ticket';
+		
+
+
+		}else if ($_GET['action']=='remove'){
+		//Remove the ticket
+		$infraction_no=$_GET['ticket'];
+		if ($infraction_no==null){
+			print "Error, ticket cannot be null";
+		} else {
+			if ($ticketinstance->deleteTicket($infraction_no)){
+				print "Ticket ".$infraction_no." removed successfully!";
+			} else {
+				print "ERROR: Ticket ".$infraction_no." could not be removed!";
+			}
+		}
+	
+
+
+		} else if ($_GET['action']=='update'){
+		//Update ticket
+		$infraction_no= $_GET['ticket'];
+		if ($infraction_no==null){
+			print "Error, ticket cannot be null";
+		} else {
+			if (isset($_GET['form'])){
+				//This is a return call from the form, we do an update on the database
+			$newTicketInfo['Client_ID'] = $_POST['fm-clientid'];
+			$newTicketInfo['Infraction_No'] = $_POST['fm-infraction_no'];
+			$newTicketInfo['Officer_Name'] = $_POST['fm-officer_name'];
+			$newTicketInfo['Officer_No'] = $_POST['fm-officer_no'];
+			$newTicketInfo['Classification'] = $_POST['fm-classification'];
+			$newTicketInfo['Date'] = $_POST['fm-year'].$_POST['fm-month'].$_POST['fm-day'];
+				
+				if ($ticketinstance->updateTicket($infraction_no,$newTicketInfo)){
+					print "Ticket ".$infraction_no." successfully updated<br />\n";
+					print "<a href=\"tickets.php?action=update&ticket=".$infraction_no."\">Return</a>\n";
+				} else {
+					print "Error occured, please check your input";
+				}
+			} else {
+				//We cant to display an update form and get information
+				$ticketinstance->printUpdateForm($infraction_no);
+			}
+		}
+	}
+	
+
+		else {
+		//Home, display a list of tickets and some statistics
+		include $includesfolder."displayticketstats.php";
+		}
+	}
+
+	else {
+	//User is either not logged in, or has no permissions
+	echo 'Access denied';
+	}
 ?>
