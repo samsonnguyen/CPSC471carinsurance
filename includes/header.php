ad52df6f033f6cbbafde560b99049b9219f66d1f
Jyro117 <Jyro117@Jyro117-PC>
2010-12-07 12:28:52 -0700

    *Cleaned up header link text *Added action='invalid' to index *Added action='invalid' to occur if menu access when not logged in

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,7 @@
 <html>
 
 <head>
-	<title>CPSC471 Car insurance managment</title>
+	<title>CPSC471 Car Insurance Management</title>
 	<link rel="stylesheet" type="text/css" href="css/page.css" />
 </head>
 <body>
@@ -26,33 +26,163 @@
 					}
 				?>
 				</a></li>
-				<li><a href="client.php">Client</a>
+				<!-- Clients -->
+				<li>
+				<?php
+					if(isLoggedIn()) {
+						echo '<a href="client.php">Clients</a>';
+					}
+					else {
+						echo '<a href="index.php?action=invalid">Clients</a>';
+					}				 
+				?>						
 					<ul>
-						<li><a href="client.php?action=add">Add Client</a></li>
+						<li>
+						<?php
+							if(isLoggedIn()) {
+								echo '<a href="client.php?action=add">Add Client</a>';
+							}
+							else {
+								echo '<a href="index.php?action=invalid">Add Client</a>';
+							}				 
+						?>								
+						</li>
 						<!-- added the search function not sure if i should remove the update and remove items above ^^^ -->
-						<li><a href="client.php?action=search">Search for Client</a></li>
+						<li>
+						<?php
+							if(isLoggedIn()) {
+								echo '<a href="client.php?action=search">Search Clients</a>';
+							}
+							else {
+								echo '<a href="index.php?action=invalid">Search Clients</a>';
+							}				 
+						?>								
+						</li>
 					</ul>
 				</li>
-				<li><a href="vehicle.php">Vehicles</a>
+				<!-- Vehicles -->
+				<li>
+				<?php
+					if(isLoggedIn()) {
+						echo '<a href="vehicle.php">Vehicles</a>';
+					}
+					else {
+						echo '<a href="index.php?action=invalid">Vehicles</a>';
+					}				 
+				?>						
 					<ul>
-						<li><a href="vehicle.php?action=add">Add new vehicle</a></li>
-						<li><a href="vehicle.php?action=search">Search for vehicle</a></li>
+						<li>
+						<?php
+							if(isLoggedIn()) {
+								echo '<a href="vehicle.php?action=add">Add Vehicle</a>';
+							}
+							else {
+								echo '<a href="index.php?action=invalid">Add Vehicle</a>';
+							}				 
+						?>								
+						</li>
+						<li>
+						<?php
+							if(isLoggedIn()) {
+								echo '<a href="vehicle.php?action=search">Search Vehicles</a>';
+							}
+							else {
+								echo '<a href="index.php?action=invalid">Search Vehicles</a>';
+							}				 
+						?>								
+						</li>
 					</ul>
 				</li>
-				<li><a href="premium.php"></a></li>
-				<li><a href="claim.php">Manage Claims</a>
+				<!-- Premiums -->
+				<!-- INCOMPLETE -->
+				<li>
+				<?php
+					if(isLoggedIn()) {
+						echo '<a href="premium.php"></a>';
+					}
+					else {
+						echo '<a href="index.php?action=invalid"></a>';
+					}				 
+				?>					
+				</li>
+				<!-- Claims -->	
+				<li>
+				<?php
+					if(isLoggedIn()) {
+						echo '<a href="claim.php">Claims</a>';
+					}
+					else {
+						echo '<a href="index.php?action=invalid">Claims</a>';
+					}				 
+				?>						
 					<ul>
-						<li><a href="claim.php?action=add">Add new claim</a></li>
-						<li><a href="claim.php?action=search">Search for Claims</a></li>
+						<li>
+						<?php
+							if(isLoggedIn()) {
+								echo '<a href="claim.php?action=add">Add Claim</a>';
+							}
+							else {
+								echo '<a href="index.php?action=invalid">Add Claim</a>';
+							}				 
+						?>							
+						</li>
+						<li>
+						<?php
+							if(isLoggedIn()) {
+								echo '<a href="claim.php?action=search">Search Claims</a>';
+							}
+							else {
+								echo '<a href="index.php?action=invalid">Search Claims</a>';
+							}				 
+						?>								
+						</li>
 					</ul>
 				</li>
-				<li><a href="tickets.php">Tickets</a>
+				<!-- Tickets -->
+				<li>
+				<?php
+					if(isLoggedIn()) {
+						echo '<a href="tickets.php">Tickets</a>';
+					}
+					else {
+						echo '<a href="index.php?action=invalid">Tickets</a>';
+					}				 
+				?>				
 					<ul>
-						<li><a href="tickets.php?action=add">Add Ticket</a></li>
-						<li><a href="tickets.php?action=search">Search for Ticket</a></li>
+						<li>
+						<?php
+							if(isLoggedIn()) {
+								echo '<a href="tickets.php?action=add">Add Ticket</a>';
+							}
+							else {
+								echo '<a href="index.php?action=invalid">Add Ticket</a>';
+							}				 
+						?>
+						</li>
+						<li>
+						<?php
+							if(isLoggedIn()) {
+								echo '<a href="tickets.php?action=search">Search Tickets</a>';
+							}
+							else {
+								echo '<a href="index.php?action=invalid">Search Tickets</a>';
+							}				 
+						?>
+						</li>
 					</ul>
 				</li>
-				<li><a href="manager.php">Managers</a></li>
+				<!-- Managers -->
+				<li>
+				<?php
+					if(isLoggedIn()) {
+						echo '<a href="manager.php">Managers</a>';
+					}
+					else {
+						echo '<a href="index.php?action=invalid">Managers</a>';
+					}				 
+				?>
+				</li>
+				<!-- Logout -->
 				<?php
 					if(isLoggedIn()){
 						echo '<li><a href="logout.php">Logout</a></li>';
--- index.php
+++ index.php
@@ -5,12 +5,16 @@ require 'config.php';
 include $includesfolder.'header.php';
 
 //Content starts here
-echo '<p>Welcome to the '.$sitename.' homepage</p>'."\n";
+echo '<p>Welcome to the '.$sitename.' homepage.</p>'."\n";
 
 if(isLoggedIn()){ //User is logged in
 	echo '<p>You are now logged in</p>'."\n";
 	echo '<a href="logout.php">Logout</a>';
-} else { //Use if not logged in present login form
+} else {  
+	if ($_GET['action']=='invalid'){ //Attempted to use menu before logging in
+		echo '<p><span style="color:red">Please log in before accessing menus.</span></p>'."\n";
+	}
+	//Use if not logged in present login form
 	include $includesfolder.'login.php';
 }
 
