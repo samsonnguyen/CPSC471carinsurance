ee4b19934d40ff27802f1b711eb7e250f2aef8fa
Jyro117 <Jyro117@192.168.0.14>
2010-12-12 11:28:35 -0700

    Setting up company and related files

--- company.php
+++ company.php
@@ -8,13 +8,26 @@ include $includesfolder.'header.php';
 
 if (isLoggedIn() && (getUserPermissions()>='1')){
 	$companyinstance = new Company();
-	$policyinstance = new Policy();
+//	$policyinstance = new Policy();
 	if ($_GET['action']=='add'){
 		include $includesfolder.'addcompany.php';
+	} elseif ($_GET['action']=='remove') {
+		// TODO Incomplete
+	} elseif ($_GET['action']=='update') {
+		// TODO Incomplete
+	} elseif ($_GET['action']=='search'){
+		if($_GET['query'] != null) {
+			// TODO Incomplete
+		} else {
+			// Not searching for something?
+			include $includesfolder.'searchcompany.php';
+		}
+	} elseif (isset($_GET['addcompany'])){
+		// TODO Incomplete
 	} else{
 		//Company home, display stats?
-		// TODO FIX!
-//		include $includesfolder.'displaypolicystats.php';
+		// TODO Incomplete
+		include $includesfolder.'displaypolicystats.php';
 	}	
 } else {
 	//user not logged in or has incorrect permissions
